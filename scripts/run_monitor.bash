6ee6e77c8fbab4d9ebdb82fabd6d10c875a28ad9
Tomáš Sobota <67016313+sobotat@users.noreply.github.com>
2023-11-10 11:48:31 +0100

    Fixed route to run

--- scripts/run_monitor.bash
+++ scripts/run_monitor.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup python -u main_monitor.py > "crash_monitor.log" 2>&1 < /dev/null &
+nohup python -u ../main_monitor.py > "crash_monitor.log" 2>&1 < /dev/null &
 echo "main_monitor.py was started"
